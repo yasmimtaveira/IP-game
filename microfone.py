9568c612d79e82882b5d366d1ee4afa7a82ee3ef
Larissa Paiva <larissapaivadossantos11@gmail.com>
2025-12-15 22:53:48 -0300

    TESTE sprite do microfone (objetos coletáveis

---
+++ microfone.py
@@ -0,0 +1,27 @@
+import os
+from random import randint
+
+
+class Microfone:
+    def __init__(self, imagem, cor_tipo):
+        # imagem: Surface do microfone
+        # cor_tipo: string apenas para identificar (azul, laranja, roxo)
+        self.imagem = imagem
+        self.tipo = cor_tipo
+        self.rect = self.imagem.get_rect()
+        self.reposicionar()
+
+    def reposicionar(self):
+        self.rect.topleft = (
+            randint(40, 600),
+            randint(50, 430)
+        )
+
+    def desenhar(self, tela):
+        tela.blit(self.imagem, self.rect)
+
+    def verificar_colisao(self, jogador_rect):
+        if self.rect.colliderect(jogador_rect):
+            self.reposicionar()
+            return True
+        return False
