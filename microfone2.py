539a96379b3b7e0baa5b56281ebed59b214f9631
Larissa Paiva <larissapaivadossantos11@gmail.com>
2025-12-16 09:18:55 -0300

    microfone  igual ao objeto
    Mudei algumas coisas da classe microfone  para tentar rodar igual o sapato

--- microfone2.py
+++ microfone2.py
@@ -1,24 +1,15 @@
-from constantes import *
-import pygame, os
+#microfne igual sapato 
+import pygame
 from random import randrange
+from constantes import *
+
 
 class Microfone(pygame.sprite.Sprite):
     def __init__(self):
-        #inicializa  a classe Sprite com init
         pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(diretorio_imagens, 'microfone.png'))
 
-        #carrega a imagem do microfone
-        self.image = pygame.image.load(os.path.join(diretorio_imagens, 'microfone.png')).convert_alpha()
-
-        #redimensionar a sprite
-        self.image = pygame.transform.scale(self.image, (80, 80))
-
-        #cria o rect
         self.rect = self.image.get_rect()
-
-        # posição inicial aleatória na tela
         self.rect.y = randrange(40, 440, 50)
         self.rect.x = randrange(50, 750, 50)
-
-        #máscara para colisão pixel-perfect (mais precisa que rect)
         self.mask = pygame.mask.from_surface(self.image)
